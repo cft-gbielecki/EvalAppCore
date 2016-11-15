543376802f1e3e5e5eb86394fc67ef9463caf65a
cft-gbielecki <grzegorz.bielecki@cfteurope.pl>
2016-11-15 15:21:52 +0100

    Entity Framework installed, dependencies added.

---
+++ src/EvalAppCore/CFT_specific/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvalAppCore.CFT_specific
+{
+    public class Constants
+    {
+    }
+
+    enum Services
+    {
+        Consult,
+        Eval/Supp,
+        L
+    }
+
+
+
+}
